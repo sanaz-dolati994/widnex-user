ad1a2623efe2f469df77e3b182a349e886e5b482
sanaz dolati <sanazdolati32@gmail.com>
2025-12-23 10:15:58 +0330

    Fix Router nesting, organize routes and add Suspense in Index.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,47 +1,44 @@
-import { lazy, Suspense, useEffect, useState } from 'react'
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
+import React, { useEffect, Suspense, lazy } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { useMainContext } from '../core/contexts/main'
 import i18next from 'i18next'
-import UserInvite from './UserInvite'
+
+// Layouts & Providers
 import GlobalLoading from '../components/layouts/loading/GlobalLoading'
-import BankCallback from './BankCallback'
 import CrispProvider from '../components/layouts/crisp/components/CrispProvider'
-import TransactionHistoryPage from './TransactionHistoryPage'
-import UserOtc from './UserOtc'
 import { SocketProvider } from '../core/contexts/socket'
 import { SocketContentProvider } from '../core/contexts/socket-content'
 import { TradePriceProvider } from '../core/contexts/trade-price'
+
+// Pages
+import MainUserDashboard from './NewMainDashboard'
+import RegisterPage from './RegisterPage'
+import TransactionHistoryPage from './TransactionHistoryPage'
 import TransfersHistoryPage from './TransfersHistoryPage'
-// import RegisterPage from './RegisterPage'
-
-const UserCurrentOrders = lazy(() => import('./UserCurrentOrders'))
-// const RegisterSignIn = lazy(() => import('./RegisterSignIn'))
-const RegisterPage = lazy(() => import('./RegisterPage'))
-const UserOrdersHistory = lazy(() => import('./UserOrdersHistory'))
-const UserOtcHistory = lazy(() => import('./UserOtcHistory'))
-const UserTransactionHistory = lazy(() => import('./UserTransactionHistory'))
-const UserAccountsAndCards = lazy(() => import('./accounts/NewUserAccounts'))
-const UserTickets = lazy(() => import('./UserTickets'))
-// const UserNotifications = lazy(() => import('./UserNotifications'))
-const UserNotifications = lazy(() => import('./NewUserNotifications'))
-const UserWallets = lazy(() => import('./NewUserWallet'))
-const DepositCoin = lazy(() => import('./wallet/deposit/coin/DepositCoin'))
-const WithdrawCoin = lazy(() => import('./wallet/withdraw/coin/WithdrawCoin'))
-const DepositFiat = lazy(() => import('./wallet/deposit/fiat/DepositFiat'))
-const WithdrawFiat = lazy(() => import('./wallet/withdraw/fiat/WithdrawFiat'))
-const UserWalletsOperation = lazy(() => import('./UserWalletsOperation'))
-const UserAuthentication = lazy(() => import('./UserAuthentication'))
-const UserSecurity = lazy(() => import('./UserSecurity'))
-const MainUserDashboard = lazy(() => import('./NewMainDashboard'))
-const UserProfile = lazy(() => import('./UserProfile'))
-const WalletCharts = lazy(() => import('./WalletCharts'))
-const UserTrace = lazy(() => import('./UserTrace'))
-const Trade = lazy(() => import('./trade/main/Trade'))
-const AdvanceTrade = lazy(() => import('./trade/main/AdvanceTrade'))
-
-const TempWallet = lazy(() => import('./wallet/NewUserWallet'))
-const StakingMain = lazy(() => import('../modules/staking/index'))
-const StakingDetail = lazy(() => import('../modules/staking/components/StakingDetail'))
+import UserOtc from './UserOtc'
+import UserCurrentOrders from './UserCurrentOrders'
+import UserOrdersHistory from './UserOrdersHistory'
+import UserOtcHistory from './UserOtcHistory'
+import UserAccountsAndCards from './accounts/NewUserAccounts'
+import UserTickets from './UserTickets'
+import UserNotifications from './NewUserNotifications'
+import UserWallets from './NewUserWallet'
+import DepositCoin from './wallet/deposit/coin/DepositCoin'
+import WithdrawCoin from './wallet/withdraw/coin/WithdrawCoin'
+import DepositFiat from './wallet/deposit/fiat/DepositFiat'
+import WithdrawFiat from './wallet/withdraw/fiat/WithdrawFiat'
+import UserWalletsOperation from './UserWalletsOperation'
+import UserAuthentication from './UserAuthentication'
+import UserSecurity from './UserSecurity'
+import UserProfile from './UserProfile'
+import WalletCharts from './WalletCharts'
+import UserTrace from './UserTrace'
+import Trade from './trade/main/Trade'
+import AdvanceTrade from './trade/main/AdvanceTrade'
+import StakingMain from '../modules/staking/index'
+import StakingDetail from '../modules/staking/components/StakingDetail'
+import UserInvite from './UserInvite'
+import BankCallback from './BankCallback'
 
 const Index = () => {
 	const {
@@ -49,11 +46,12 @@ const Index = () => {
 		profile: { token },
 	} = useMainContext()
 
+	// تنظیم زبان
 	useEffect(() => {
 		i18next.changeLanguage(lang)
-		//eslint-disable-next-line
-	}, [])
+	}, [lang])
 
+	// تنظیم تم
 	useEffect(() => {
 		if (theme === 'dark') {
 			document.documentElement.classList.add('dark')
@@ -62,80 +60,65 @@ const Index = () => {
 		}
 	}, [theme])
 
+	// تعریف همه مسیرهای برنامه
+	const routes = [
+		{ path: '/current-orders', element: <UserCurrentOrders /> },
+		{ path: '/orders-history', element: <UserOrdersHistory /> },
+		{ path: '/otc-history', element: <UserOtcHistory /> },
+		{ path: '/otc', element: <UserOtc /> },
+		{ path: '/accounts-cards', element: <UserAccountsAndCards /> },
+		{ path: '/tickets', element: <UserTickets /> },
+		{ path: '/notifications', element: <UserNotifications /> },
+		{ path: '/wallets', element: <UserWallets /> },
+		{ path: '/wallets/:subpage', element: <UserWallets /> },
+		{ path: '/transaction-history', element: <TransactionHistoryPage /> },
+		{ path: '/transfers-history', element: <TransfersHistoryPage /> },
+		{ path: '/wallets/depositCoin', element: <DepositCoin /> },
+		{ path: '/wallets/withdrawCoin', element: <WithdrawCoin /> },
+		{ path: '/wallets/depositFiat', element: <DepositFiat /> },
+		{ path: '/wallets/withdrawFiat', element: <WithdrawFiat /> },
+		{ path: '/wallet-operations', element: <UserWalletsOperation /> },
+		{ path: '/authentication', element: <UserAuthentication /> },
+		{ path: '/security', element: <UserSecurity /> },
+		{ path: '/profile', element: <UserProfile /> },
+		{ path: '/profile/:subpage', element: <UserProfile /> },
+		{ path: '/invite', element: <UserInvite /> },
+		{ path: '/callback', element: <BankCallback /> },
+		{ path: '/wallet-charts', element: <WalletCharts /> },
+		{ path: '/log', element: <UserTrace /> },
+		{ path: '/trade', element: <Trade /> },
+		{ path: '/trade/advance', element: <AdvanceTrade /> },
+		{ path: '/staking', element: <StakingMain /> },
+		{ path: '/staking/:id', element: <StakingDetail /> },
+		{ path: '/staking-history', element: <StakingMain /> },
+	]
+
 	return (
-		<Router basename={process.env.PUBLIC_URL}>
-			<Suspense fallback={<GlobalLoading />}>
-				<CrispProvider>
-					<SocketProvider>
-						<SocketContentProvider>
-							<TradePriceProvider>
-								{token ? (
-									<Routes>
-										<Route index path='/dashboard' element={<MainUserDashboard />} />
-
-										{routes.map((route) => (
-											<Route path={route.path} element={route.element} />
-										))}
-
-										<Route path='*' element={<Navigate to='/dashboard' replace />} />
-									</Routes>
-								) : (
-									<Routes>
-										<Route
-											path='/register-signin'
-											element={
-												// <RegisterSignIn />
-												<RegisterPage />
-											}
-										/>
-										<Route
-											path='*'
-											element={<Navigate to='/register-signin?mode=login' replace />}
-										/>
-									</Routes>
-								)}
-							</TradePriceProvider>
-						</SocketContentProvider>
-					</SocketProvider>
-				</CrispProvider>
-			</Suspense>
-		</Router>
+		<Suspense fallback={<GlobalLoading />}>
+			<CrispProvider>
+				<SocketProvider>
+					<SocketContentProvider>
+						<TradePriceProvider>
+							{token ? (
+								<Routes>
+									<Route index path='/dashboard' element={<MainUserDashboard />} />
+									{routes.map((route) => (
+										<Route key={route.path} path={route.path} element={route.element} />
+									))}
+									<Route path='*' element={<Navigate to='/dashboard' replace />} />
+								</Routes>
+							) : (
+								<Routes>
+									<Route path='/register-signin' element={<RegisterPage />} />
+									<Route path='*' element={<Navigate to='/register-signin?mode=login' replace />} />
+								</Routes>
+							)}
+						</TradePriceProvider>
+					</SocketContentProvider>
+				</SocketProvider>
+			</CrispProvider>
+		</Suspense>
 	)
 }
-
-const routes = [
-	{ path: '/current-orders', element: <UserCurrentOrders /> },
-	{ path: '/orders-history', element: <UserOrdersHistory /> },
-	{ path: '/otc-history', element: <UserOtcHistory /> },
-	{ path: '/otc', element: <UserOtc /> },
-	// { path: '/transaction-history', element: <UserTransactionHistory /> },
-	{ path: '/accounts-cards', element: <UserAccountsAndCards /> },
-	{ path: '/tickets', element: <UserTickets /> },
-	// { path: '/notifications', element: <UserNotifications /> },
-	{ path: '/notifications', element: <UserNotifications /> },
-	{ path: '/wallets_old', element: <TempWallet /> },
-	{ path: '/wallets', element: <UserWallets /> },
-	{ path: '/wallets/:subpage', element: <UserWallets /> },
-	{ path: '/transaction-history', element: <TransactionHistoryPage /> },
-	{ path: '/transfers-history', element: <TransfersHistoryPage /> },
-	{ path: '/wallets/depositCoin', element: <DepositCoin /> },
-	{ path: '/wallets/withdrawCoin', element: <WithdrawCoin /> },
-	{ path: '/wallets/depositFiat', element: <DepositFiat /> },
-	{ path: '/wallets/withdrawFiat', element: <WithdrawFiat /> },
-	{ path: '/wallet-operations', element: <UserWalletsOperation /> },
-	{ path: '/authentication', element: <UserAuthentication /> },
-	{ path: '/security', element: <UserSecurity /> },
-	{ path: '/profile', element: <UserProfile /> },
-	{ path: '/profile/:subpage', element: <UserProfile /> },
-	{ path: '/invite', element: <UserInvite /> },
-	{ path: '/callback', element: <BankCallback /> },
-	{ path: '/wallet-charts', element: <WalletCharts /> },
-	{ path: '/log', element: <UserTrace /> },
-	{ path: '/trade', element: <Trade /> },
-	{ path: '/trade/advance', element: <AdvanceTrade /> },
-	{ path: '/staking', element: <StakingMain /> },
-	{ path: '/staking/:id', element: <StakingDetail /> },
-	{ path: '/staking-history', element: <StakingMain /> },
-]
 
 export default Index
